6c2f7187a7cfbb20cdfa6acc415cc65a6ceb2ea9
Tim Sneddon <tim@sneddon.id.au>
2021-03-12 02:59:18 -0500

    Moved to Apache 2.0 license

--- lib/extrn.h
+++ lib/extrn.h
@@ -1,3 +1,28 @@
+/*
+** extrn.h
+**
+**  Prototypes for internal functions used by the TECO interpreter.
+**
+**  Copyright 2021, Tim E. Sneddon <tim@sneddon.id.au>
+**
+**  Licensed under the Apache License, Version 2.0 (the "License");
+**  you may not use this file except in compliance with the License.
+**  You may obtain a copy of the License at
+**
+**    http://www.apache.org/licenses/LICENSE-2.0
+**
+**  Unless required by applicable law or agreed to in writing, software
+**  distributed under the License is distributed on an "AS IS" BASIS,
+**  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+**  See the License for the specific language governing permissions and
+**  limitations under the License.
+**
+**  CREATION DATE:      25-SEP-2012
+**
+**  MODIFICATION HISTORY:
+**
+**      25-SEP-2012 V41.00  Sneddon   Initial coding.
+*/
 #ifndef __TECO_EXTRN_LOADED
 #define __TECO_EXTRN_LOADED 1
 #include "tecodef.h"
--- lib/globals.h
+++ lib/globals.h
@@ -4,20 +4,19 @@
 **  External reference definitions for TECO globals.  These are defined as
 **  globals in teco.c.
 **
-**  Copyright (C) 2012, Tim E. Sneddon <tim@sneddon.id.au>
+**  Copyright 2021 Tim E. Sneddon <tim@sneddon.id.au>
 **
-**  This program is free software: you can redistribute it and/or modify
-**  it under the terms of the GNU General Public License as published by
-**  the Free Software Foundation, either version 3 of the License, or
-**  (at your option) any later version.
+**  Licensed under the Apache License, Version 2.0 (the "License");
+**  you may not use this file except in compliance with the License.
+**  You may obtain a copy of the License at
 **
-**  This program is distributed in the hope that it will be useful,
-**  but WITHOUT ANY WARRANTY; without even the implied warranty of
-**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**  GNU General Public License for more details.
+**    http://www.apache.org/licenses/LICENSE-2.0
 **
-**  You should have received a copy of the GNU General Public License
-**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**  Unless required by applicable law or agreed to in writing, software
+**  distributed under the License is distributed on an "AS IS" BASIS,
+**  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+**  See the License for the specific language governing permissions and
+**  limitations under the License.
 **
 **  CREATION DATE:      25-SEP-2012
 **
@@ -25,8 +24,8 @@
 **
 **      25-SEP-2012 V41.00  Sneddon   Initial coding.
 */
-#ifndef globals_h__
-#define globals_h__
+#ifndef __TECO_GLOBALS_LOADED
+#define __TECO_GLOBALS_LOADED 1
 #include "tecodef.h"
 
     extern TECODEF      ctx;               /* TECO's context area      */
@@ -35,4 +34,4 @@
 
     extern QRGDEF       qreg_local[];
 
-#endif /* globals_h__ */
+#endif /* __TECO_GLOBALS_LOADED */
